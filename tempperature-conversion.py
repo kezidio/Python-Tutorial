af2e539ae4cb325279d156478ab09f836e36ee27
Kaweny Ezidio <143475552+kezidio@users.noreply.github.com>
2026-04-16 02:13:38 +0200

    Add files via upload

---
+++ tempperature-conversion.py
@@ -0,0 +1,10 @@
+#declare a variable and ask user to enter the temperature in F to be converted
+tempF = input("Enter temperature in F:")
+ 
+#declare variable and calculate temperature  conversion to celsius
+tempC= (float(tempF)-32)*5/9
+
+#display temperature in C
+print("The tempture in C is", temC)
+
+
