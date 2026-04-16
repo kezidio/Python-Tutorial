a6eaf19c0ec455b098d2a786cf9bf93fc25bac6e
Kaweny Ezidio <143475552+kezidio@users.noreply.github.com>
2026-04-16 05:38:42 +0200

    Add files via upload

---
+++ final_grade.py
@@ -0,0 +1,15 @@
+#A simple program that asks user to enter their grade and
+#calculates the final grade usinf a while loop
+
+
+final_grade:0
+m = int(input("How many assignments did you complete? "))
+m = n
+while n!= 0:
+	grade =float(input("Enter grade: "))
+	final_grade = final_grade + grade
+	n-1
+
+final_grade=final_grade/m
+print("Your final grade is :",final_grade)
+
