3c2130312e311bb7e2efa14099b2057e48f44ac1
Kaweny Ezidio <143475552+kezidio@users.noreply.github.com>
2026-04-16 13:33:27 +0200

    Update random_numbers.py

--- random_numbers.py
+++ random_numbers.py
@@ -1,6 +1,10 @@
-
+#This is a a simple program that uses Python’s 
+#built-in random module to generate random numbers, allowing 
+#the user to produce unpredictable values for testing, 
+#simulations, or basic games.
 
 import random
+
 def main():
     # Get a random number.
     number = random.randint(1, 10)
