acbb0b84f5150300728ec67b3401e6f3357150a9
Kaweny Ezidio <143475552+kezidio@users.noreply.github.com>
2026-04-16 05:16:12 +0200

    Add files via upload

---
+++ power_of_two.py
@@ -0,0 +1,15 @@
+#A simple program that asks the user for a number of iterations and then prints powers of 2 from 
+# 2^0 to 2^n. It uses a while loop to incrementally compute and display each value.
+# Author: Kaweny Ezidio
+
+#declare a variable to store the amount of numbers to print
+n = int(input("Enter a positive number of iterations: "))
+
+#declare the initial iteration
+i=0
+
+#create a while loop to prin n calculations
+while i<=n:
+	#display 2^i
+	print(2**i)
+	i=i+1 #adding 1 to i makes the loop keep going
